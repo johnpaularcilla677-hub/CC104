2825daf0e76dd49e404c21c92ccfe13297744929
johnpaularcilla677-hub <johnpaularcilla677@gmail.com>
2025-09-30 15:56:05 +0800

    Add files via upload

---
+++ (SEPT.2,25)JP WAi.py
@@ -0,0 +1,21 @@
+import random
+
+def main():
+    """ Inputs the bound of the range of numbers
+    and lets the user guess the computer's number until
+    the guess is correct."""
+    smaller=int (input("Enter the smaller number:"))
+    larger = int (input("enter the larger number:"))
+    myNumber = random.randint (smaller,larger)
+    count=0
+    while True:
+        count+=1
+        userNumber=int(input("enter your guess:"))
+        if userNumber <myNumber:
+            print ("Too small")
+        elif userNumber > myNumber:
+            print ("Too large")
+        else:
+            print("You've got it in",count,"tries!")
+if __name__ == "__main__":
+    main()
---
+++ Lists.py
@@ -0,0 +1,16 @@
+testlist = []
+testlist.append(34)
+testlist.append(22)
+testlist.sort()
+testlist.pop()
+testlist.insert(0,22)
+testlist.insert(1,55)
+testlist.pop(1)
+testlist.remove(22)
+testlist.remove(55)
+print(testlist)
+
+
+
+
+
